5700829619b89bd9c41247b3177324f624da54a6
Екатерина Благочинная <e.blagochinnaya@dnevnik.ru>
2024-12-26 17:30:08 +0200

    Добавьте файлы проекта.

---
+++ Laba 6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MatrixLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество строк (n):");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите количество столбцов (m):");
+        int m = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[n, m];
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        int[] resultColumn = MatrixOperations.GetColumnWithMinSum(matrix);
+        Console.WriteLine("Столбец с минимальной суммой элементов:");
+        foreach (var item in resultColumn)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ MatrixLibrary/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MatrixLibrary
+{
+    public class MatrixOperations
+    {
+        public static int[] GetColumnWithMinSum(int[,] matrix)
+        {
+            int columns = matrix.GetLength(1);
+            int minSumIndex = -1;
+            int minSum = int.MaxValue;
+
+            for (int j = 0; j < columns; j++)
+            {
+                int sum = 0;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    sum += matrix[i, j];
+                }
+                if (sum < minSum)
+                {
+                    minSum = sum;
+                    minSumIndex = j;
+                }
+            }
+
+            int[] resultColumn = new int[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                resultColumn[i] = matrix[i, minSumIndex];
+            }
+
+            return resultColumn;
+        }
+    }
+}
